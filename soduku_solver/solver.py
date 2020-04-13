c852443ac5e19a182d0fafc773420eb3390c33cf
Johan Vergeer <johanvergeer@gmail.com>
2020-04-13 20:15:53 +0200

    remove print_board

--- soduku_solver/solver.py
+++ soduku_solver/solver.py
@@ -16,18 +16,3 @@ def solve(board: Board) -> bool:
         board.unset(pos)
 
     return False
-
-
-def print_board(bo: Board):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - -")
-
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print("| ", end="")
-
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
